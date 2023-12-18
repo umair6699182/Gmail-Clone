ad69181dd8feb4073c859e735a08eb25a3d123c1
Umair Malik <um83414@gmail.com>
2023-12-18 20:10:25 +0500

    config update

--- database/config.js
+++ database/config.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-
+dotenv.config();
 
 const Connection = async ()=>{
+    const URL = `mongodb+srv://${process.env.User}:${process.env.Password}@gmail-clone.lk1yc4u.mongodb.net/?retryWrites=true&w=majority`
     try {
-        await mongoose.connect(process.env.DATABASE_URL );
+        await mongoose.connect(URL);
         console.log("Database Connected SuccessFully")
     } catch (error) {
         console.log("Database Connected Error", error)
