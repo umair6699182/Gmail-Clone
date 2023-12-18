68a326202255db77687299b47f7bb25cb63d6178
Umair Malik <um83414@gmail.com>
2023-12-18 19:45:04 +0500

    build added folder

---
+++ clinet/build/static/js/367.52504a97.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclinet=self.webpackChunkclinet||[]).push([[367],{5367:(e,n,l)=>{l.r(n),l.d(n,{default:()=>t});l(2791);var c=l(697),r=l(890),s=l(7689),o=l(184);function t(){const e=(0,s.lk)();return console.log(e),(0,o.jsx)(c.Z,{children:(0,o.jsx)(r.Z,{children:"There was an error whlie loading a components"})})}}}]);
+//# sourceMappingURL=367.52504a97.chunk.js.map
